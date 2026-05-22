d07b1b80bbd18b5fdbf468c1bb01580267511c4a
bhushan310793 <bhushanbabar710@gmail.com>
2026-05-22 13:32:26 +0000

    updated

--- main.tf
+++ main.tf
@@ -1,54 +1,49 @@
+
 terraform {
-    required_providers {
-          aws = {
-                  source  = "hashicorp/aws"
-                        version = "~> 5.0"
-                            }
-                              }
-                            }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
-                            provider "aws" {
-                                region = "us-east-1"
-                              }
+provider "aws" {
+  region = "us-east-1"
+}
 
-                              resource "aws_security_group" "demo_sg" {
-                                  name = "terraform-demo-sg"
+resource "aws_security_group" "demo_sg" {
+  name        = "terraform-demo-sg-v2"
+  description = "Allow SSH and HTTP access"
 
-                                    ingress {
-                                          description = "SSH"
-                                              from_port   = 22
-                                                  to_port     = 22
-                                                      protocol    = "tcp"
-                                                          cidr_blocks = ["0.0.0.0/0"]
-                                                            }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-                                                              ingress {
-                                                                    description = "HTTP"
-                                                                        from_port   = 80
-                                                                            to_port     = 80
-                                                                                protocol    = "tcp"
-                                                                                    cidr_blocks = ["0.0.0.0/0"]
-                                                                                      }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-                                                                                        egress {
-                                                                                              from_port   = 0
-                                                                                                  to_port     = 0
-                                                                                                      protocol    = "-1"
-                                                                                                          cidr_blocks = ["0.0.0.0/0"]
-                                                                                                            }
-                                                                                                          }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
-                                                                                                          resource "aws_instance" "demo" {
-                                                                                                              ami           = "ami-0c02fb55956c7d316"
-                                                                                                                instance_type = "t3.micro"
+resource "aws_instance" "demo" {
+  ami                    = "ami-0c02fb55956c7d316"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.demo_sg.id]
 
-                                                                                                                  security_groups = [aws_security_group.demo_sg.name]
-
-                                                                                                                    tags = {
-                                                                                                                          Name = "terraform-free-tier-demo"
-                                                                                                                            }
-                                                                                                                          }
-
-                                                                                                                          output "public_ip" {
-                                                                                                                              value = aws_instance.demo.public_ip
-                                                                                                                            }
+  tags = {
+    Name = "github-actions-demo"
+  }
+}
